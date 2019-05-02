b7ad5db79796d51c93b6ceef25fd6072e67c21c6
Louis <canalbloglouissama@gmail.com>
2019-05-02 11:32:51 +0200

    ça marche

--- src/Bassez/Bortolotti/Desmarescaux/sample/Main.java
+++ src/Bassez/Bortolotti/Desmarescaux/sample/Main.java
@@ -1,24 +1,45 @@
 package Bassez.Bortolotti.Desmarescaux.sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) {
+        final Circle circle_1 = new Circle(400, 100,100);//x,y,rayon
+        circle_1.setFill(Color.RED);
+
+        //Tentative de mettre un deuxieme point
+
+        final Circle circle_2 = new Circle(100, 100,100);//x,y,rayon
+        circle_2.setFill(Color.GREEN);
+
+        //Mise en place de texte
+        final Text text = new Text("Salut le monde !");
+        text.setLayoutX(100);
+        text.setLayoutY(100);
+        text.setFill(Color.BLUE);
+
+        //Fenetre
+
+        final Pane root = new Pane();
+        root.getChildren().setAll(circle_1,circle_2,text);//Doit contenir tous les noeuds
+        final Scene scene = new Scene(root, 600, 600);
+        //Taille de la fenetre d'affichage
+        primaryStage.setTitle("Affichage de plusieurs cercles");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
 //test
